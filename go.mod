1f6a7e45c50c856bf31a96220a153bcd329a5b85
kinggo <lilong.21@bytedance.com>
2023-02-02 14:25:52 +0800

    feat: add http2curl

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/li-jin-gou/http2curl
+
+go 1.19
+
+require (
+	github.com/cloudwego/hertz v0.5.1
+	github.com/valyala/fasthttp v1.44.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/bytedance/gopkg v0.0.0-20220413063733-65bf48ffb3a7 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
---
+++ http2curl_fasthttp.go
@@ -0,0 +1,55 @@
+package http2curl
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/valyala/fasthttp"
+)
+
+// GetCurlCommandFastHttp returns a CurlCommand corresponding to an http.Request
+func GetCurlCommandFastHttp(req *fasthttp.Request) (*CurlCommand, error) {
+	if req.URI() == nil {
+		return nil, ErrorURINull
+	}
+
+	command := CurlCommand{}
+
+	command.append("curl")
+
+	requestURL := req.URI().String()
+
+	if string(req.URI().Scheme()) == "https" {
+		command.append("-k")
+	}
+
+	command.append("-X", bashEscape(b2s(req.Header.Method())))
+
+	body := req.Body()
+	if len(body) != 0 {
+		bodyEscaped := bashEscape(b2s(body))
+		command.append("-d", bodyEscaped)
+
+	}
+
+	headers := make(map[string][]string)
+
+	req.Header.VisitAll(func(key, value []byte) {
+		_, ok := headers[b2s(key)]
+		if !ok {
+			headers[b2s(key)] = []string{b2s(value)}
+		} else {
+			headers[b2s(key)] = append(headers[b2s(key)], b2s(value))
+		}
+	})
+
+	for key, values := range headers {
+		command.append("-H", bashEscape(fmt.Sprintf("%s: %s", key, strings.Join(values, " "))))
+	}
+
+	command.append(bashEscape(requestURL))
+
+	command.append("--compressed")
+
+	return &command, nil
+}
